ba2b3c2ad8e62d0dc7cd7cd1b7dfe116ffc31d85
Nethmina Sandaruwan <125788788+ByteBigBoss@users.noreply.github.com>
2024-09-25 12:47:22 +0530

    add types

--- types/shoelace.d.ts
+++ types/shoelace.d.ts
@@ -2,3 +2,4 @@ declare module '@shoelace-style/shoelace/dist/utilities/base-path' {
     export function setBasePath(path: string): void;
   }
   
+declare module '@shoelace-style/shoelace/dist/react/animation'
